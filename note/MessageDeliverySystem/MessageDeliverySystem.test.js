0899db81d051572eaee3d03fa6d5d76eb02ad315
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-04-05 12:22:55 +0200

    [fixing environment from REPL]

--- note/MessageDeliverySystem/MessageDeliverySystem.test.js
+++ note/MessageDeliverySystem/MessageDeliverySystem.test.js
@@ -1,19 +1,19 @@
-// const { describe, it, expect } = require("jest");
-const { describe } = require("node:test");
+const { test, expect } = require("jest");
 const messageDeliverySystem = require("./MessageDeliverySystem.js");
 
-describe
-describe("UnitTests", function () {
-  test("MessageDeliverySystem", async function () {
-    it("should pass test case 1", () => {
-      const testCase1 = {
-        timestamps: [1, 4, 5, 10, 11, 14],
-        messages: ["hello", "bye", "bye", "hello", "bye", "hello"],
-        k: 5,
-      };
-      const expected1 = ["true", "true", "false", "true", "true", "false"];
+// describe("MessageDeliverySystem", async function () {
+//   it("should pass test case 1", () => {
+//     const testCase1 = {
+//       timestamps: [1, 4, 5, 10, 11, 14],
+//       messages: ["hello", "bye", "bye", "hello", "bye", "hello"],
+//       k: 5,
+//     };
+//     const expected1 = ["true", "true", "false", "true", "true", "false"];
 
-      expect(messageDeliverySystem(testCase1)).toEqual(expected1);
-    });
-  });
+//     expect(messageDeliverySystem(testCase1)).toEqual(expected1);
+//   });
+// });
+
+test("test", () => {
+  console.log("?");
 });
