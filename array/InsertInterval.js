5fd93d6968defb0fa9d458298aa9d4e068efb42d
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-05-08 12:28:56 +0200

    [leetcode] array Insert Interval

--- array/InsertInterval.js
+++ array/InsertInterval.js
@@ -17,4 +17,30 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-var insert = function (intervals, newInterval) {};
+var insert = function (intervals, newInterval) {
+  const result = [];
+  let i = 0;
+
+  // Add intervals before newInterval
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  // Merge overlapping intervals
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+
+  result.push(newInterval);
+
+  // Add intervals after newInterval
+  while (i < intervals.length) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  return result;
+};
